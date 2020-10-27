f28b46c840c0273463f2badfdc444e02d6ce9ed8
Sanjit Kumar <sanjitk2018@gmail.com>
2020-10-27 22:35:47 +0530

    added update product feature

--- src/components/Dashboard/Products.js
+++ src/components/Dashboard/Products.js
@@ -2,14 +2,18 @@ import React from 'react'
 import axios from 'axios'
 import Cookies from 'js-cookie'
 import FormData from 'form-data'
+import qs from 'qs'
 
 function Products() {
     const [products, setProducts] = React.useState()
     const final = new FormData();
     const [loading, setLoading] = React.useState(true)
     const [update, setUpdate] = React.useState(0)
+    const [edit, setEdit] = React.useState(0)
+    const [editing, setEditing] = React.useState(0)
     const [uploading, setUploading] = React.useState(0)
     const [picture, setPicture] = React.useState()
+    const [clickedEditProduct, setClickedEditProduct ] = React.useState({})
     // const [error, setError] = React.useState()
     const [newProduct, setNewProduct] = React.useState({
         pCount: "",
@@ -21,6 +25,15 @@ function Products() {
         pDesc: "",
         pPicture: ""
     })
+
+    const [editedProduct, setEditedProduct] = React.useState({
+        pCount: "",
+        pAvailability: "",
+        pPriceEst: "",
+        pDesc: ""
+    })
+
+
     React.useEffect(() => {
         axios.get("http://127.0.0.1:4000/products/summary").then(response => {
             setProducts(response.data)
@@ -51,10 +64,38 @@ function Products() {
         })
 
     }
+    // Here
+    function HandleProductEdit(product) {
+        const id = product.pCode;
+        setEdit(1)
+        axios.patch("http://127.0.0.1:4000/products/" + id, {
+            headers: {
+                'Authorization': 'Bearer ' + Cookies.get("auth")
+            }
+        }).then(response => {
+            console.log(response.status);
+
+            setLoading(true)
+        })
+
+    }
+
     function HandleChange(event) {
         const name = event.target.name
         const value = event.target.value
         setNewProduct((previous) => {
+            return {
+                ...previous,
+                [name]: value
+            }
+        })
+    }
+    // Here
+    function HandleEditProductChange(event) {
+        const name = event.target.name
+        const value = event.target.value
+        console.log("editedProduct from HandleEditProductChange:",editedProduct)
+        setEditedProduct((previous) => {
             return {
                 ...previous,
                 [name]: value
@@ -102,6 +143,41 @@ function Products() {
         setUploading(0)
     }
 
+    // Edited Product
+    function HandleEditSubmit() {
+        // console.log(newProduct);
+        console.log("editedProduct from HandleEditSubmit:",qs.stringify(editedProduct))
+
+        console.log("final from HandleEditSubmit:",final)
+        setEditing(1)
+        console.log("stuck");
+        // if (!(picture === undefined || picture === null)) {
+        console.log("success");
+        console.log("this is thisClicked in handleEditSubmit", clickedEditProduct.pCode)
+        axios.patch("http://127.0.0.1:4000/products/" + clickedEditProduct.pCode ,qs.stringify(editedProduct),{
+            headers: {
+                'Authorization': 'Bearer ' + Cookies.get("auth"),
+                "Content-Type": "application/x-www-form-urlencoded"
+            }  
+        }).then(response => {
+            setLoading(true);
+            setEditing(2);
+            console.log(response.status);
+        }).catch(err => {
+            console.log(err)
+            setEditing(3)
+        })
+        // } else {
+        //     document.getElementById("error").innerHTML = "Please check your fields and try again!"
+        // }
+
+        console.log(final);
+    }
+
+    function HandleEditClose() {
+        setEditing(0)
+    }
+
     return (
         <>
             <button type="button" className="btn btn-primary mb-3" data-toggle="modal" data-target="#exampleModal">
@@ -116,9 +192,9 @@ function Products() {
                             <th>Color</th>
                             <th>Count</th>
                             <th>Quantity</th>
-                            <th>Count</th>
                             <th>Price</th>
                             <th>Delete</th>
+                            <th>Edit</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -130,12 +206,16 @@ function Products() {
                                     <td>{product.pColor}</td>
                                     <td>{product.pCount}</td>
                                     <td>{product.pQty}</td>
-                                    <td>{product.pCount}</td>
                                     <td>{product.pPriceEst}</td>
                                     {update === 0 ?
                                         <td><button className="btn btn-danger" onClick={() => HandleClick(product)}>Delete</button></td>
                                         :
                                         <td><button className="btn btn-danger" onClick={() => HandleClick(product)}>Deleting</button></td>
+                                    }
+                                    {edit === 0 ?
+                                        <td><button className="btn btn-primary" onClick={() => setClickedEditProduct(product)} data-toggle="modal" data-target="#editProductModal">Edit</button></td>
+                                        :
+                                        <td><button className="btn btn-primary" data-toggle="modal" data-target="#editProductModal">Updating</button></td>
                                     }
                                 </tr>)
                         }) : <td>{update === 0 ? "Loading..." : update === 2 && "Unable to fetch data"}</td>}
@@ -169,6 +249,32 @@ function Products() {
                                 uploading === 1 ? <button type="button" class="btn btn-light" onClick={HandleSubmit}>Uploading</button> :
                                     uploading === 2 ? <button type="button" class="btn btn-success" data-dismiss="modal" onClick={HandleClose}>Done</button> :
                                         uploading === 3 && <button type="button" class="btn btn-danger" onClick={HandleSubmit}>Try Again</button>}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal fade" id="editProductModal" tabindex="-1" aria-labelledby="editProductModal" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="editProductModalTitle">Update the following product fields</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <input className="form-control mb-2" type="number" placeholder="Product Count" name="pCount" onChange={HandleEditProductChange}></input>
+                            <input className="form-control mb-2" type="text" placeholder="Availability" name="pAvailability" onChange={HandleEditProductChange}></input>
+                            <input className="form-control mb-2" type="number" placeholder="Estimated Price" name="pPriceEst" onChange={HandleEditProductChange}></input>
+                            <input className="form-control mb-2" type="test" placeholder="Product Description" name="pDesc" onChange={HandleEditProductChange}></input>
+                            <p id="error"></p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal" onClick={HandleEditClose}>Close</button>
+                            {editing === 0 ? <button type="button" class="btn btn-primary" onClick={HandleEditSubmit}>Update Product</button> :
+                            editing === 1 ? <button type="button" class="btn btn-light" onClick={HandleEditSubmit}>Uploading</button> :
+                            editing === 2 ? <button type="button" class="btn btn-success" data-dismiss="modal" onClick={HandleEditClose}>Done</button> :
+                            editing === 3 && <button type="button" class="btn btn-danger" onClick={HandleEditSubmit}>Try Again</button>}
                         </div>
                     </div>
                 </div>
